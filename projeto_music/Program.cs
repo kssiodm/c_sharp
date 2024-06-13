8aa226ab84aeaa17271605a6f5c4feaf7a841baa
kssiodm <cassiodemaz@gmail.com>
2024-06-13 14:56:26 -0300

    update

---
+++ projeto_music/Program.cs
@@ -0,0 +1,15 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Believer";
+musica1.artista = "Imagine dragons";
+musica1.duracao = 439;
+musica1.disponivel = true;
+
+
+Musica musica2 = new Musica();
+musica2.nome = "Gangsta's paradise";
+musica2.artista = "Coolio";
+musica2.duracao = 569;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
