165bf40a69e601bb7e201dcde27b2b864a520d81
kssiodm <cassiodemaz@gmail.com>
2024-06-17 14:29:21 -0300

    update podcast

--- projeto_music/Program.cs
+++ projeto_music/Program.cs
@@ -1,27 +1,40 @@
-﻿Banda queen = new Banda("Queen");
+﻿// Banda queen = new Banda("Queen");
+
+// Album albumDoQueen = new Album("A night at the opera");
+
+// Musica musica1 = new Musica(queen, "Love of my life")
+// {
+//     Duracao = 213,
+//     Disponivel = true
+// };
+
+// Musica musica2 = new Musica(queen, "Bohemiam Rhapsody")
+// {
+//     Duracao = 354,
+//     Disponivel = false
+// };
+
+// albumDoQueen.AdicionarMusica(musica1);
+// albumDoQueen.AdicionarMusica(musica2);
+// queen.AdcionarAlbum(albumDoQueen);
 
 
-Album albumDoQueen = new Album("A night at the opera");
+// musica1.ExibirFichaTecnica();
+// musica2.ExibirFichaTecnica();
+// albumDoQueen.ExibirMusicasAlbum();
+// queen.ExibirDiscografia();
 
-Musica musica1 = new Musica(queen, "Love of my life")
-{
-    Duracao = 213,
-    Disponivel = true
-};
+Episodio ep1 = new(1, "Técnicas de facilitação", 45);
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("Marcelo");
 
-Musica musica2 = new Musica(queen, "Bohemiam Rhapsody")
-{
-    Duracao = 354,
-    Disponivel = false
-};
+Episodio ep2 = new(2, "Técnicas de apredizado", 67);
+ep2.AdicionarConvidados("Fernando");
+ep2.AdicionarConvidados("Marcos");
+ep2.AdicionarConvidados("Flávia");
 
-albumDoQueen.AdicionarMusica(musica1);
-albumDoQueen.AdicionarMusica(musica2);
-queen.AdcionarAlbum(albumDoQueen);
-
-
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
-albumDoQueen.ExibirMusicasAlbum();
-queen.ExibirDiscografia();
+Podcast podcast= new("Podcast especial", "Daniel");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.ExibirDetalhes();
 
