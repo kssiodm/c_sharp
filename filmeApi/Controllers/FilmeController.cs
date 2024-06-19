76a0a44cb67ae13fae324a18e09c13d747ff4f75
kssiodm <cassiodemaz@gmail.com>
2024-06-19 14:38:19 -0300

    filme API

---
+++ filmeApi/Controllers/FilmeController.cs
@@ -0,0 +1,25 @@
+using filmeApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace filmeApi.controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody]Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Duracao);
+        }
+
+    private class ApiControllerAttribute : Attribute
+    {
+    }
+}
+
